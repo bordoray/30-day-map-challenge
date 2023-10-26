d02f37f787993492fd434c2f10ffd62d342d29c6
bordoray <lay.ishizuka@gmail.com>
2023-10-26 14:40:25 +0900

    Svelte

---
+++ 2023/src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<div class="heading">
+  <h1>#30DayMapChallenge 2023 Game</h1>
+</div>
+
+<style>
+  .heading {
+    margin: 0;
+    padding: 0px;
+    background-color: white;
+    color: black;
+  }
+
+  .heading > h1 {
+    padding: 20px;
+    margin: 0;
+  }
+</style>
